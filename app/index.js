6def30b2decd83565f5d1e33bd846f295378f45f
Omar Melendrez <omar.melendrez@gmail.com>
2023-01-08 18:17:13 -0300

    Remove cors options

--- app/index.js
+++ app/index.js
@@ -4,11 +4,7 @@ require('dotenv').config()
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:8000"
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
